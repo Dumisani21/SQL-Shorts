d83f8bcfe4399bcf543ae4f1e664fec04346d9a3
Dumisani21 <76276338+Dumisani21@users.noreply.github.com>
2022-01-19 09:48:30 +0200

    fix bugs

--- database/sqlite.py
+++ database/sqlite.py
@@ -159,23 +159,3 @@ class DATABASE:
 		print("view data is returned")
 			
 		return list_data
-
-
-DB = DATABASE('Company.db')
-
-
-# DB.sql_create_table('customer',"(fname text,lname text,email text,phone integer)")
-
-# DB.sql_insert_data('customer',('Bob','Doe','JamesD@email.com',4123021391))
-
-# DB.sql_udate_data('customer',"email = 'MaxS@email.com'",1)
-
-# DB.sql_delete_data('customer',2)
-
-# DB.sql_drop_table('customer')
-
-# print(DB.sql_view_all('customer'))
-
-# print(DB.sql_view_specific('customer','fname, email'))
-
-# DB.sql_search_table('customer',"rowid = 2 AND fname = 'Bob'")
